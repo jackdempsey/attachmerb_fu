6bc11cc581958daecfe9b189635f02fcfcc7bc9a
Ho-Sheng Hsiao <hosh@isshen.com>
2008-07-08 16:38:51 -0400

    Factored out the class rewriting code

---
+++ datamapper-bindings.rb
@@ -0,0 +1,16 @@
+require "attachmerb_fu"
+#require "geometry"
+#require "tempfile_ext"
+
+# make sure we're running inside Merb
+if defined?(Merb::Plugins)
+
+  # Merb gives you a Merb::Plugins.config hash...feel free to put your stuff in your piece of it
+  Merb::Plugins.config[:attachmerb_fu] = {
+  }
+  
+  #Merb::Plugins.add_rakefiles "attachmerb_fu/merbtasks"
+  
+  DataMapper::Base.send(:extend, AttachmerbFu::ActMethods)
+  
+end
--- lib/attachmerb_fu.rb
+++ lib/attachmerb_fu.rb
@@ -430,17 +430,3 @@ module AttachmerbFu # :nodoc:
       end
   end
 end
-
-    
-# make sure we're running inside Merb
-if defined?(Merb::Plugins)
-
-  # Merb gives you a Merb::Plugins.config hash...feel free to put your stuff in your piece of it
-  Merb::Plugins.config[:attachmerb_fu] = {
-  }
-  
-  #Merb::Plugins.add_rakefiles "attachmerb_fu/merbtasks"
-  
-  DataMapper::Base.send(:extend, AttachmerbFu::ActMethods)
-  
-end
